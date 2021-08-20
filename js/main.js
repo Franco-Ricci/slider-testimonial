169b7bafa0ae77e08da94d1dbb69ce3b8ed39236
Franco <francoricci_93@hotmail.com>
2021-08-20 13:04:44 -0300

    alt img

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@ prev.addEventListener("click", () => {
 
 button = () => {
   if (numb == true) {
-    imagesUsers.setAttribute("src", "../images/image-john.jpg");
+    imagesUsers.setAttribute("src", "/images/image-john.jpg");
     textHeader.innerHTML ="“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
     subt.innerHTML = "John Tarkpor";
     job.innerHTML = "Junior Front-end Developer";
     numb = false;
   } else if (numb == false) {
-    imagesUsers.setAttribute("src", "../images/image-tanya.jpg");
+    imagesUsers.setAttribute("src", "/images/image-tanya.jpg");
     textHeader.innerHTML ="“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”";
     subt.innerHTML = "Tanya Sinclair";
     job.innerHTML = "UX Engineer";
